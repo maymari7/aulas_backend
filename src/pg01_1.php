c6bba659fe70076b0b78357ccbfbec87256d203a
ⓜⓐⓡⓘⓔ <160788603+maymari7@users.noreply.github.com>
2026-03-12 14:34:30 +0000

    aula - 120326

---
+++ src/pg01_1.php
@@ -0,0 +1,25 @@
+
+<?php
+echo "<h1>Tópico 02 </h1>";
+print "funciona<br>";
+
+$idade=25;//int
+$nota=8.5;//float
+$nome="marie";//string
+
+echo $nome, " voce tem ", $idade, " anos e sua nota é ", $nota;
+echo "<hr>";
+echo "$nome voce tem <b>$idade anos</b> e sua nota é $nota";
+
+echo "<hr>";
+
+var_dump($idade);
+var_dump($nome);
+
+echo "<hr>";
+
+echo phpversion()
+
+?>
+<p>ablueblueblue</p>
+
---
+++ src/pg02_1.php
@@ -0,0 +1,57 @@
+<h1>Pg02_1 - 12/03/2026</h1>
+<?php
+    $idade = 34;
+    echo "idade = $idade<br>";
+    $cep = "01012123"; 
+    echo "cep = $cep<br>";
+
+    /*
+    bases:
+decimal - 0123456789 10
+binário - 01 10
+octal - 01234567 10
+hexadecimal 0123456789abcdef 10
+    */
+
+    $preco = 5247.543; //tipo de dado - float
+    /*
+como transformar um numero de programção para o numero que o usuário entende?
+dolar uusa .
+real usa ,
+    echo number format(); - Formata um numero, é necessário mostrar a ele quantas casas ele precisa, e alterar virgulas e pontos
+    passo a passo:
+    echo number_format($preco); saida: preco = R$ 5,248
+    echo number_format($preco ,2); saida: preco = R$ 5,247.54
+    echo number_format($preco ,2,","); saida: preco = R$ 5,247,54
+    echo number_format($preco ,2,",","."); saida: preco = R$ 5.247,54
+    */
+    echo "preco = R$ ";
+    echo number_format($preco ,2,",","."); 
+
+//diferença de aspas simples e duplas
+echo "<hr>";
+    $cor ="purple";
+    echo "cor = $cor<br>";
+    echo 'cor = $cor<br>';
+echo "<hr>";
+    echo "<h2 style='color:$cor'>outro titulo 2</h2>";
+    $ativo = true; //váriavel verdadeira o falso NAO usa aspas ("true" errado)
+    echo " está ativo? $ativo <br>";
+    echo "<hr>";
+
+//concatenação usar o ponto (.)
+    echo "aquela era: " .$cor;
+    echo "<hr>";
+
+//qual é o valor da nota em numeros inteiros?
+    //transformação de numero flutuante para numeros inteiros, com concatenação
+    $nota=7.8;
+    echo "nota = ".(int)$nota; 
+    echo "<br>";
+//mostrar apenas os centavos
+    $notadecimal=9.5; //valor cheio menos a parte inteira (9.5 - 5 = 0.5)
+    echo "nota = " .$notadecimal - (int)$notadecimal;
+
+
+?>
+
